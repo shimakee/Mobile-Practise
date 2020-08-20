971ccfe78fc7a54f1bbd2958463932838d59bb1c
Shimakee <shimakee@gmail.com>
2020-08-20 15:30:37 +0800

    Added Images and UI Manager
    added UI controller, UI manager, and UI switcher.

---
+++ Assets/Scripts/Components/managers/UiManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public enum UiType
+{
+    mainMenu,
+    options,
+    something
+}
+
+public class UiManager : MonoBehaviour
+{
+    List<UiController> CanvasControllerList;
+    UiController lastActiveCanvas;
+    private void Awake()
+    {
+        CanvasControllerList = GetComponentsInChildren<UiController>().ToList();
+        CanvasControllerList.ForEach(item => item.gameObject.SetActive(false));
+
+        SwitchCanvas(UiType.mainMenu);
+    }
+
+    public void SwitchCanvas(UiType canvasType)
+    {
+        if (lastActiveCanvas != null)
+            lastActiveCanvas.gameObject.SetActive(false);
+
+        UiController desiredCanvas = CanvasControllerList.Find(item => item.CanvasType == canvasType);
+        if(desiredCanvas != null)
+        {
+            lastActiveCanvas = desiredCanvas;
+            desiredCanvas.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Components/managers/UiSwitcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UiSwitcher : MonoBehaviour
+{
+    public UiType desiredUi;
+
+    UiManager UiManager;
+    Button Button;
+
+    private void Start()
+    {
+        Button = GetComponent<Button>();
+        Button.onClick.AddListener(OnButtonClicked);
+        UiManager = FindObjectOfType<UiManager>();
+    }
+
+    void OnButtonClicked()
+    {
+        UiManager.SwitchCanvas(desiredUi);
+    }
+}
